e89bfb7de4d2ad3492b56b5c2226083255ccf19e
manuel-spec <amaniase084@gmail.com>
2025-04-26 14:11:33 +0300

    fix: availablity user included

--- src/tasks/availability/availability.service.js
+++ src/tasks/availability/availability.service.js
@@ -1,7 +1,19 @@
+const { User } = require("../../auth/model/user.model.js");
 const { Availability } = require("../models/availability.model.js");
 const _ = require("lodash");
 
-const getAllAvailabilitiesService = async () => await Availability.findAll();
+const getAllAvailabilitiesService = async () => {
+    const av = await Availability.findAll({
+        include: [
+            {
+                model: User,
+                as: "technician",
+            },
+        ],
+    });
+    if (_.isEmpty(av)) return;
+    return av;
+};
 const getAvailabilityByIdService = async (id) => {
     const av = await Availability.findByPk(id);
     if (_.isEmpty(av)) return;
